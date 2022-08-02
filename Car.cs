de69d05dd14b3bf6d0152ef699168742030bc686
nolto1sj <sjnolton@gmail.com>
2022-08-01 21:25:34 -0400

    Created Car Class

---
+++ Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsedCarLab
+{
+    public class Car
+    {
+       public Car(string make, string model, int year, decimal price)
+        {
+            Make = make;
+            Model = model; 
+            Year = year;  
+            Price = price;
+        }
+
+        //properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+        public Car() { }
+    }
+}
